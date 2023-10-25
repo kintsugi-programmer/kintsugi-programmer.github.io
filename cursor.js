c20807689bafc313b496b01643fdbf3d10c31197
Siddhant Bali <siddhant22496@iiitd.ac.in>
2023-10-26 03:44:40 +0530

    Links updated

--- cursor.js
+++ cursor.js
@@ -1,14 +1,17 @@
+// Get all anchor (link) elements on the page
 const links = document.getElementsByTagName("a");
+
+// Initialize the currentLinkIndex to -1 (no link focused initially)
 let currentLinkIndex = -1;
 
 // Function to handle arrow key events
 function handleArrowKeys(event) {
     switch (event.key) {
         case "ArrowUp":
-            navigateLinks(-1); // Move to the previous link
+            navigateLinks(-1); // Move to the previous link when the up arrow key is pressed
             break;
         case "ArrowDown":
-            navigateLinks(1); // Move to the next link
+            navigateLinks(1); // Move to the next link when the down arrow key is pressed
             break;
     }
 }
@@ -19,17 +22,17 @@ function navigateLinks(step) {
 
     // Ensure the index doesn't go out of bounds
     if (currentLinkIndex < 0) {
-        currentLinkIndex = 0;
+        currentLinkIndex = 0; // Don't go below the first link
     } else if (currentLinkIndex >= links.length) {
-        currentLinkIndex = links.length - 1;
+        currentLinkIndex = links.length - 1; // Don't go beyond the last link
     }
 
     // Focus on the selected link
     links[currentLinkIndex].focus();
 }
 
-// Add an event listener to capture arrow key events
+// Add an event listener to capture arrow key events when any key is pressed
 document.addEventListener("keydown", handleArrowKeys);
 
-// Initialize focus on the first link
+// Initialize focus on the first link by moving to the next link (1)
 navigateLinks(1);
